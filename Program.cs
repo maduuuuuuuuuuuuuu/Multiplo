9e9eab613112817990d1c0ddf362f38b68219dfb
madu <maduamaral2006@gmail.com>
2023-09-02 16:22:10 -0300

    exercício resolvido

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double numero, multiplo, resultado;
+Console.WriteLine("Multiplo");
+Console.WriteLine("Digite um numero");
+numero = Convert.ToDouble(Console.ReadLine()!);
+Console.WriteLine("Avaliar se é multiplo de:");
+multiplo = Convert.ToDouble(Console.ReadLine()!);
+resultado = numero % multiplo;
+
+if(resultado == 0){
+    Console.WriteLine($"{numero} é multiplo de {multiplo}");
+}
+else{
+    Console.WriteLine($"{numero} não é multiplo de {multiplo}");
+}
+
+
+
